500feefbb3fe027e961731c6901b338ee3f4affc
LuisAlfaro96 <alfaro.13.luis@gmail.com>
2022-05-18 11:19:15 -0600

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "server_ami" { #a datasource its basically a query of the aws API to receive information  needed to deploy a resource, in this case an AMI for use later
+data "aws_ami" "server_ami" { #a datasource its basically a query of the aws API to receive information  needed to deploy a resource, in this case an AMI to use later
     most_recent = true
     owners = ["099720109477"]
     filter {
